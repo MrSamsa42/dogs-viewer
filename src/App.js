5874023696552d2f7cdbe0155486c701337ee074
Jonathan Adler <jradler@gmail.com>
2019-04-18 09:21:15 -0700

    connect to API and save data to state

--- src/App.js
+++ src/App.js
@@ -2,6 +2,33 @@ import React, { Component } from 'react';
 //import './App.css';
 
 class App extends Component {
+  state = {
+    data: {}, 
+  }
+
+  async componentDidMount() {
+    try {
+      const response = await fetch('https://dog.ceo/api/breeds/list/all');
+      const json = await response.json();
+      this.setState({data: json.message})
+      console.log("Here is the data...");
+      console.log(this.state.data);
+    } catch(error) {
+      console.log(error);
+    }
+
+    try {
+      let affenpinscher = Object.keys(this.state.data)[0];
+      const response = await fetch(`https://dog.ceo/api/breed/${affenpinscher}/images`);
+      const json = await response.json();
+      console.log("Pics of a single breed....");
+      console.log(json);
+    } catch(error) {
+      console.log(error);
+    }    
+  }
+  
+
   render() {
     return (
       <div className="App">
